d7002d1d5143117788283ffd4709ddcb726530b4
Łukasz Kogut <77071211+Kropekkk@users.noreply.github.com>
2022-12-16 16:39:41 +0100

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,42 @@
+import torch
+from config import (NUM_CLASSES, TRAIN_DIR, ANN_DIR)
+from model import get_model_instance_segmentation
+import cv2
+from utils import collate_fn, get_train_transform
+from dataset import CustomDataset
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+model = get_model_instance_segmentation(NUM_CLASSES)
+checkpoint = torch.load('outputs/last_model.pth', map_location=device)
+model.load_state_dict(checkpoint['model_state_dict'])
+model.to(device).eval()
+
+data_transform = get_train_transform()
+dataset = CustomDataset(data_transform, TRAIN_DIR, ANN_DIR)
+data_loader = torch.utils.data.DataLoader(
+dataset, batch_size=4, collate_fn=collate_fn)
+
+for imgs, annotations in data_loader:
+        imgs = list(img.to(device) for img in imgs)
+        annotations = [{k: v.to(device) for k, v in t.items()} for t in annotations]
+        break
+preds = model(imgs)
+
+def plot_image(img_tensor, annotation):
+    img = torch.permute(img_tensor, (1,2,0))
+    img_np = img.to('cpu').numpy()
+
+    img_np1 = img_np*255
+
+    im_rgb = cv2.cvtColor(img_np1, cv2.COLOR_RGB2BGR)
+    
+    for box in annotation["boxes"]:
+        xmin, ymin, xmax, ymax = box
+        
+        cv2.rectangle(im_rgb, (int(xmin),int(ymin)),(int(xmax),int(ymax)), (0,255,0),2)
+    
+    cv2.imwrite('prediction.png', im_rgb)
+
+plot_image(imgs[2],preds[2])
+print("Save predicition.png")
