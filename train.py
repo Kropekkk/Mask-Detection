e966d9b7c435ad41484b08c10e616c4e86889193
Łukasz Kogut <77071211+Kropekkk@users.noreply.github.com>
2022-12-17 14:41:03 +0100

    Update train.py

--- train.py
+++ train.py
@@ -7,7 +7,14 @@ from config import (NUM_CLASSES, NUM_EPOCHS, BATCH_SIZE, LEARNING_RATE, MOMENTUM
 if __name__ == '__main__':
   data_transform = get_train_transform()
   dataset = CustomDataset(data_transform, TRAIN_DIR, ANN_DIR)
-  dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, collate_fn=collate_fn)
+  train_size = int(len(dataset) * 0.8)
+  test_size = len(dataset) - train_size
+  train_data, test_data = torch.utils.data.random_split(dataset,[train_size,test_size])
+
+  print(f"Train dataset lenght: {len(train_data)}   Test data: {len(test_data)} ")
+  
+  train_dataloader = torch.utils.data.DataLoader(train_data, batch_size=BATCH_SIZE, collate_fn=collate_fn)
+  test_dataloader = torch.utils.data.DataLoader(test_data, batch_size=BATCH_SIZE, collate_fn=collate_fn)
 
   device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
@@ -17,23 +24,25 @@ if __name__ == '__main__':
   params = [p for p in model.parameters() if p.requires_grad]
   optimizer = torch.optim.SGD(params, lr=LEARNING_RATE, momentum=MOMENTUM, weight_decay=WEIGHT_DECAY)
 
-  train_loss = []
+  train_loss_results = []
 
   for epoch in range(NUM_EPOCHS):
     model.train() 
-    epoch_loss = 0
-    for imgs, annotations in dataloader:
+    train_loss = 0
+    test_loss = 0
+
+    for imgs, annotations in train_dataloader:
       imgs = list(img.to(device) for img in imgs)
       annotations = [{k: v.to(device) for k, v in t.items()} for t in annotations]
-      loss_dict = model([imgs[0]], [annotations[0]])
-      losses = sum(loss for loss in loss_dict.values())        
+      train_loss_dict = model([imgs[0]], [annotations[0]])
+      train_losses = sum(loss for loss in train_loss_dict.values())        
 
       optimizer.zero_grad()
-      losses.backward()
+      train_losses.backward()
       optimizer.step() 
-      epoch_loss += losses
-    
-    train_loss.append(float(epoch_loss))
+      train_loss += train_losses
 
+    train_loss_results.append(train_loss)
+    
   save_model(model, optimizer)
-  save_train_loss(train_loss)
+  save_train_loss(train_loss_results)
