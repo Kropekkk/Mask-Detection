48c8ad118681df5addf583fc1efb4c6e527bc73e
Łukasz Kogut <77071211+Kropekkk@users.noreply.github.com>
2023-01-05 20:13:24 +0100

    upload images

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .forms import ImageUploadForm
 
-# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            #transform image and make predicitions
+            pass
+    else:
+        form = ImageUploadForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'app/index.html', context)
