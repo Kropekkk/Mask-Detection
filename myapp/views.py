94517c36a62026e08105481d582d62ae68c181d5
Łukasz Kogut <77071211+Kropekkk@users.noreply.github.com>
2023-01-09 12:59:04 +0100

    Update app urls

--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
 from .forms import ImageUploadForm
+from .detect import predict
 
 def index(request):
+    path_img = None
+
     if request.method == 'POST':
         form = ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            #transform image and make predicitions
+            image = form.cleaned_data['image']
+            image_bytes = image.file.read()
+
+            try:
+                path_img = predict(image_bytes)
+                
+            except RuntimeError as re:
+                print(re)
+
             pass
     else:
         form = ImageUploadForm()
 
     context = {
         'form': form,
+        'image_uri': path_img,
     }
-    return render(request, 'app/index.html', context)
+
+    return render(request, 'myapp/index.html', context)
