7b132d1e7c746f850106b48dbf18068f578627b5
Łukasz Kogut <77071211+Kropekkk@users.noreply.github.com>
2023-01-09 12:32:16 +0100

    Add upgraded prediction modules

--- train/utils.py
+++ train/utils.py
@@ -1,6 +1,8 @@
 import torch
 from torchvision import transforms
 import matplotlib.pyplot as plt
+import cv2
+import numpy as np
 
 def collate_fn(batch):
     return tuple(zip(*batch))
@@ -29,3 +31,41 @@ def save_loss(train_loss, test_loss):
     plt.legend()
     plt.savefig("train_loss.png")
     plt.close('all')
+
+def plot_image(img, annotation, scale_h, scale_w, save = False):
+    """
+    Draw rectangles on original image
+    """
+    img_np = np.asarray(img)
+
+    img_rgb = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+
+    for box in annotation['boxes']:
+        xmin, ymin, xmax, ymax = box
+        
+        cv2.rectangle(img_rgb, (int(xmin*scale_w), int(ymin*scale_h)), (int(xmax*scale_w), int(ymax*scale_h)), (0,255,0), 2)
+    
+    if save:
+        cv2.imwrite('prediction.png', img_rgb)
+
+    return img_rgb
+
+def plot_image_tensor(img_tensor, annotation, save = False):
+    """
+    Draws rectangles on detected boxes from given tensor of an image.
+
+    Saves and returns plotted image.
+    """
+    img = torch.permute(img_tensor, (1,2,0))
+    img_np = img.to('cpu').numpy()
+    img_rgb = cv2.cvtColor(img_np*255, cv2.COLOR_RGB2BGR)
+    
+    for box in annotation['boxes']:
+        xmin, ymin, xmax, ymax = box
+        
+        cv2.rectangle(img_rgb, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0,255,0), 2)
+    
+    if save:
+        cv2.imwrite('prediction.png', img_rgb)
+
+    return img_rgb
