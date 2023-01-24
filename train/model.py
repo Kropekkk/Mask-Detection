52097ca1088b9cc2d1a24233dc7cd385e0756f04
Łukasz Kogut <77071211+Kropekkk@users.noreply.github.com>
2023-01-24 15:03:08 +0100

    lighter model

--- train/model.py
+++ train/model.py
@@ -1,8 +1,13 @@
 import torchvision
+from torchvision.models.detection import FasterRCNN_MobileNet_V3_Large_FPN_Weights
+from torchvision.models import MobileNet_V3_Large_Weights
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 def get_model_instance_segmentation(num_classes):
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn(
+        weights=FasterRCNN_MobileNet_V3_Large_FPN_Weights.DEFAULT, 
+        weights_backbone=MobileNet_V3_Large_Weights.DEFAULT)
+
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
